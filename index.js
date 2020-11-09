6888639cdfa2241c8b98fc218a43818ad766a9f1
Naufal <naufalibnu75@gmail.com>
2020-11-10 00:48:20 +0700

    makesmeDESPERATE

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const bodyParser = require("body-parser");
 
 
 mongooseconfig()
+
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors())
 app.use(approuter)
 
-
+app.use.get("/", (req, res) => {
+    res.send("HELLYEAAH")
+})
 app.listen(port, () => {
     console.log(`listen on http://localhost:${port}`)
 })
