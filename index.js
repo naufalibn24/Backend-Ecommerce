31837d13b64712b06eebc849251abc7343cfc9d2
Naufal <naufalibnu75@gmail.com>
2020-11-22 11:34:48 +0700

    desperate anyyinngg

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors())
 app.use(approuter)
-
+app.get('/', (req, res) => {
+    res.send('alhamdulillah sok mangga di ango we');
+})
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
